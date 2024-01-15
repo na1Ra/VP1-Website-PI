fde3658c3f1921850df78e218a1abd5392164a6c
basux <bastienhanel@googlemail.com>
2024-01-15 10:32:44 +0100

    so ist dioch vesser

--- DHT11-Sensor-Script.py
+++ DHT11-Sensor-Script.py
@@ -2,23 +2,17 @@ import time
 import board
 import adafruit_dht
 
-dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
+dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
-def get_temp_humidity():
+while True:
     try:
         temperature_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        return temperature_c, humidity
+        print("Temp: {:.1f} C Humidity: {}% ".format(temperature_c, humidity))
     except RuntimeError as error:
         print(error.args[0])
-        return None, None
+        time.sleep(2.0)
+        continue
     except Exception as error:
         dhtDevice.exit()
         raise error
-
-# Außerhalb der Funktion
-while True:
-    temp, hum = get_temp_humidity()
-    if temp is not None and hum is not None:
-        print("Temp: {:.1f} C Humidity: {}% ".format(temp, hum))
-    time.sleep(2.0)  # Wartezeit von 2 Sekunden zwischen den Messungen

1538eef47c3ce766a0e5b545b8450fe68d63a879
na1Ra <36802580+na1Ra@users.noreply.github.com>
2024-01-15 10:29:26 +0100

    Update DHT11-Sensor-Script.py

--- DHT11-Sensor-Script.py
+++ DHT11-Sensor-Script.py
@@ -2,17 +2,22 @@ import time
 import board
 import adafruit_dht
 
-dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
+dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
 
-while True:
-    try:
-        temperature_c = dhtDevice.temperature
-        humidity = dhtDevice.humidity
-        print("Temp: {:.1f} C Humidity: {}% ".format(temperature_c, humidity))
-    except RuntimeError as error:
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
+def get_temp_humidity():
+    while True:
+        try:
+            temperature_c = dhtDevice.temperature
+            humidity = dhtDevice.humidity
+            return temperature_c, humidity
+        except RuntimeError as error:
+            print(error.args[0])
+            time.sleep(2.0)
+            continue
+        except Exception as error:
+            dhtDevice.exit()
+            raise error
+
+# Außerhalb der Funktion
+temp, hum = get_temp_humidity()
+print("Temp: {:.1f} C Humidity: {}% ".format(temp, hum))

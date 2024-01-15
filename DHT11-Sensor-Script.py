c63a505794c3f7b4977ca8dbb2b540dcb932131d
basux <bastienhanel@googlemail.com>
2024-01-15 10:12:36 +0100

    Sensor funkionirt

--- DHT11-Sensor-Script.py
+++ DHT11-Sensor-Script.py
@@ -7,9 +7,8 @@ dhtDevice = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 while True:
     try:
         temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
         humidity = dhtDevice.humidity
-        print("Temp: {:.1f} F / {:.1f} C Humidity: {}% ".format(temperature_f, temperature_c, humidity))
+        print("Temp: {:.1f} C Humidity: {}% ".format(temperature_c, humidity))
     except RuntimeError as error:
         print(error.args[0])
         time.sleep(2.0)

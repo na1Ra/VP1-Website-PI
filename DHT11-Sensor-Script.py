08b46fb043f7210acc3cc3d217acdd11ebdc0cd6
na1Ra <36802580+na1Ra@users.noreply.github.com>
2024-01-16 17:37:34 +0100

    Update DHT11-Sensor-Script.py

--- DHT11-Sensor-Script.py
+++ DHT11-Sensor-Script.py
@@ -1,21 +1,26 @@
 import adafruit_dht
 import board
 import time
+from datetime import datetime
 
 def read_dht_sensor(pin):
     # DHT-Sensor initialisieren
     dht_sensor = adafruit_dht.DHT11(pin)
 
     # HTML-Datei öffnen
-    with open('output.html', 'w') as f:
+    with open('/var/www/html/output.html', 'w') as f:
         while True:
             try:
                 # Temperatur und Luftfeuchtigkeit auslesen
                 temperature_c = dht_sensor.temperature
                 humidity = dht_sensor.humidity
 
-                # Werte in HTML-Datei schreiben
-                f.write(f"<p>Temperatur: {temperature_c}°C, Luftfeuchtigkeit: {humidity}%</p>\n")
+                # Aktuelle Uhrzeit abrufen
+                now = datetime.now()
+                current_time = now.strftime("%H:%M:%S")
+
+                # Werte und Uhrzeit in HTML-Datei schreiben
+                f.write(f"<p>Uhrzeit: {current_time}, Temperatur: {temperature_c}°C, Luftfeuchtigkeit: {humidity}%</p>\n")
                 f.flush()
 
             except RuntimeError as e:
@@ -31,4 +36,3 @@ def read_dht_sensor(pin):
 
 # Beispielaufruf der Funktion
 read_dht_sensor(board.D4)
-

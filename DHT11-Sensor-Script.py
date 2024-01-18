d7ee66825353f0218f43acba6068615f05a63312
na1Ra <bastienhanel@googlemail.com>
2024-01-18 15:08:07 +0100

    losgeths

--- DHT11-Sensor-Script.py
+++ DHT11-Sensor-Script.py
@@ -21,7 +21,7 @@ def read_dht_sensor(pin):
             current_time = now.strftime("%H:%M:%S")
 
             # Werte und Uhrzeit zur Liste hinzufügen
-            data.append(f'<div style="background-color:gray; width:100px; height:40px; position:relative;"><div style="background-color:red; width:{temperature_c}px; height:40px;"></div><span style="position:absolute; left:5px; color:white;">{temperature_c}°C</span></div>;<div style="background-color:gray; width:100px; height:40px; position:relative;"><div style="background-color:blue; width:{humidity}px; height:40px;"></div><span style="position:absolute; left:5px; color:white;">{humidity}%</span></div>')
+            data.append(f'<div style="background-color:gray; width:100%; height:20%; position:relative;"><div style="background-color:red; width:{temperature_c}%; height:100%;"></div><span style="position:absolute; left:5px; color:white;">{temperature_c}°C</span></div>;<div style="background-color:gray; width:100%; height:20%; position:relative;"><div style="background-color:blue; width:{humidity}%; height:100%;"></div><span style="position:absolute; left:5px; color:white;">{humidity}%</span></div>')
 
             # Wenn mehr als 8 Einträge in der Liste sind, den ältesten Eintrag entfernen
             if len(data) > 1:
